6f2a37262a98de100777c7a193e84457d68f06a0
adharsh <lordgamingyt648@gmail.com>
2025-04-09 14:33:55 +0530

    chore(package): added execa

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@
 import fs from "fs";
 import path from "path";
 import { execa } from "execa";
+import { fileURLToPath } from "url";
 
 import chalk from "chalk";
 import inquirer from "inquirer";
 import figlet from "figlet";
 import { createSpinner } from "nanospinner";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const templatepath = path.dirname(__dirname, "../template");
+
+console.log(templatepath);
